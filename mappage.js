fb00e6759b583208f1f9f2b7b9defad5312f2a7d
Alexrodriguez2090 <54908770+Alexrodriguez2090@users.noreply.github.com>
2019-10-16 14:38:42 -0500

    Add files via upload

--- mappage.js
+++ mappage.js
@@ -1,5 +1,17 @@
 var map;
-var findMe = {lat:-36.84,lng:174.7};
+var findMe = [
+    {"content":"Auckland, New Zealand", "coordinates":{"lat":-36.8485,"lng":174.7633}, "iconImagePath":"one.png"},
+    {"content":"Caguas, Puerto Rico", "coordinates":{"lat":18.2341,"lng":-66.0485}, "iconImagePath":"two.png"},
+    {"content":"Orlando, Florida", "coordinates":{"lat":28.4790,"lng":-81.4685}, "iconImagePath":"flag.png"},
+    {"content":"Seattle, Washington", "coordinates":{"lat":47.6062,"lng":-122.3321}, "iconImagePath":"flag.png"},
+    {"content":"Tokyo, Japan", "coordinates":{"lat":35.6762,"lng":139.6503}, "iconImagePath":"flag.png"},
+    {"content":"Los Angeles, California", "coordinates":{"lat":34.0522,"lng":-118.2437}, "iconImagePath":"flag.png"},
+    {"content":"Nelson, New Zealand", "coordinates":{"lat":-41.2706,"lng":173.2840}, "iconImagePath":"flag.png"},
+    {"content":"Krakow, Poland", "coordinates":{"lat":50.0647,"lng":19.9450}, "iconImagePath":"flag.png"},
+];
+
+var currentPlaceIndex = 7
+var currentPlace = findMe[currentPlaceIndex];
 
 // Initialization of map
 function initMap() {
@@ -14,7 +26,7 @@ function initMap() {
 	map.addListener('zoom_changed', function() {
 		var zoom = map.getZoom();
 		console.log('Zoom: '+zoom);
-		document.getElementById("Hint").value = zoom;
+		document.getElementById("Hint").value = currentPlaceIndex;
 	});
 	//Reports bounds whenever they change
 	map.addListener('bounds_changed', function() {
@@ -35,16 +47,20 @@ function initMap() {
 function doYouSeeIt() {
 	var bounds = map.getBounds();
 	var zoom = map.getZoom();
-	if (bounds.contains(findMe) && zoom < 8) {
+	// Will repeat this message if you see Auckland but are not zoomed in enough
+	if (bounds.contains(currentPlace.coordinates) && zoom < 8) {
 		console.log("---------It is in view---------");
 		console.log("--------------but--------------");
 		console.log("-------you gotta zoom in-------");
 	};
-	if (bounds.contains(findMe) && zoom >= 8) {
+	// Will repeat this message if you see Auckland and are zoomed in enough
+	if (bounds.contains(currentPlace.coordinates) && zoom >= 8) {
 		console.log("*******************************");
 		console.log("---------You found it!---------");
 		console.log("*******************************");
-		var marker = new google.maps.Marker({position: findMe, map: map});
-		findMe = {lat:41.60,lng:-88.07};
+		var marker = new google.maps.Marker({position: currentPlace.coordinates, map: map});
+		currentPlaceIndex = currentPlaceIndex - 1;
+		currentPlace = findMe[currentPlaceIndex];
+
 	};
 }
